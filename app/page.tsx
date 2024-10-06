b9067a88445167d239b18ee1b03ca70f144fd79b
Rizwana <159348120+rizwanaperveen@users.noreply.github.com>
2024-10-06 21:16:19 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  DigitalClockComponent  from '@/components/ui/clock'
+const Home = () => {
+  return (
+    <div>
+      <DigitalClockComponent />
+    </div>
+  )
+}
+
+export default Home
